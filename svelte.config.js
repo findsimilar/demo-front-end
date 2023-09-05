8371f702d098d7fe68eb346513a5d54f436a6b37
quillcraftsman <quill@craftsman.lol>
2023-09-05 22:27:01 +0400

    change logo path

--- svelte.config.js
+++ svelte.config.js
@@ -18,13 +18,14 @@ const dev = process.argv.includes('dev');
 /** @type {import('@sveltejs/kit').Config} */
 const adapter_options = {
   pages: 'demo-front-end',
-  //assets: 'demo-front-end',
+  assets: 'demo-front-end',
 }
 const config = {
   kit: {
     adapter: adapter(adapter_options),
     paths: {
       base: dev ? '' : process.env.BASE_PATH,
+      //base: '/demo-front-end'
     }
   }
 };
