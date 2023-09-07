047921be27aa682098b21b472e1c70165c23250a
quillcraftsman <quill@craftsman.lol>
2023-09-07 17:01:55 +0400

    add deploy on production

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,10 @@
 import adapter from '@sveltejs/adapter-static';
 
-const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
-const adapter_options = {
-  pages: 'demo-front-end',
-  assets: 'demo-front-end',
-}
 const config = {
   kit: {
-    adapter: adapter(adapter_options),
-    paths: {
-      base: dev ? '' : process.env.BASE_PATH,
-      //base: '/demo-front-end'
-    }
+    adapter: adapter(),
   }
 };
 
