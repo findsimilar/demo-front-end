7df8b753445dc8987c57b5ae95c345b73c352f34
quillcraftsman <quill@craftsman.lol>
2023-09-16 13:08:40 +0400

    Decompose simple page to components

---
+++ src/routes/simple/Results.svelte
@@ -0,0 +1,32 @@
+<script>
+    import {
+        Card,
+        CardHeader,
+        CardTitle,
+        CardBody,
+        ListGroup,
+        ListGroupItem,
+    } from 'sveltestrap'
+    export let result_similars
+    export let text
+</script>
+
+
+{#await result_similars}
+<p>...waiting</p>
+{:then result_similars_list}
+<Card class="mt-3">
+  <CardHeader>
+    <CardTitle>{text}</CardTitle>
+  </CardHeader>
+  <CardBody>
+    <ListGroup>
+      {#each result_similars_list as result_similar, index}
+        <ListGroupItem>{index}) {result_similar.text} - ({result_similar.cos})</ListGroupItem>
+      {/each}
+    </ListGroup>
+  </CardBody>
+</Card>
+{:catch error}
+<p style="color: red">{error.message}</p>
+{/await}
