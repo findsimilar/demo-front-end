233cd57c1fc12e795ccea1cb19d70a93d7cdfa6e
quillcraftsman <quill@craftsman.lol>
2023-09-17 20:00:09 +0400

    change production url to domain name

--- src/routes/tutorial/core.js
+++ src/routes/tutorial/core.js
@@ -2,7 +2,9 @@ import { dev } from '$app/environment'
 
 export async function fetch_data(url, method, data) {
         const api_url = `api${url}`
-        const full_url = dev ? `http://127.0.0.1:8000/${api_url}` : `http://188.64.12.238:8000/${api_url}`
+        //const production_url = 'http://188.64.12.238:8000/'
+        const production_url = 'http://api.findsimilar.org/'
+        const full_url = dev ? `http://127.0.0.1:8000/${api_url}` : `${production_url}${api_url}`
         let params = {
             headers: {
                 'Accept': 'application/json',
