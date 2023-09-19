7cbbb2c3231d5900cfcdea61ba74f25e8b2ac916
quillcraftsman <quill@craftsman.lol>
2023-09-19 16:49:25 +0400

    add mock_responses to work without backend and examples template

--- src/routes/tutorial/Tutorial.svelte
+++ src/routes/tutorial/Tutorial.svelte
@@ -7,12 +7,30 @@
     import { fetch_data } from './core.js'
     
     import {
-        Button
+        Button,
+        Dropdown,
+        DropdownItem,
+        DropdownMenu,
+        DropdownToggle
     } from 'sveltestrap'
 
     async function fetch_example(name) {
+
+        const mocked_result = {
+            text: 'This is mocked text',
+            texts: [
+                'This is mocked text too',
+                'This is other text',
+                'And other text',
+                'Again other text',
+                'Some different mocked text',
+                'Just text',
+                'End text'
+            ]
+        }
+
         const url = `/examples/${name}/`
-        return fetch_data(url, 'GET')
+        return fetch_data(url, 'GET', 'undefined', mocked_result)
     }
 
     let example = {}
@@ -31,29 +49,66 @@
 
     const default_separator = $separator
 
+    let current_example = {
+        name: '1. Simple Usage',
+        query_name: 'films-demo'
+    }
+
+    const example_list = [
+        {
+            name: '1. Simple Usage',
+            query_name: 'films-demo',
+        },
+        {
+            name: '2. Using Stop Words',
+            query_name: 'films-desctiprion',
+        }
+    ]
+
 </script>
 
 <div class="mt-1">
-    {#await example}
-    <p>...waiting</p>
-    {:then example}
-    This is the most simple example. <br>You can compare one text to anothers and find similars.
-    <p></p>
-    Input text to compare to the <b>Main text</b> input.<br>
-    Any text will be good. For example: <b>
-        {example.text}
-    </b>.<p></p>
-    Input other texts to compare and find similars to the <b>List of texts</b> input.<br>
-    For example: <b>
+    <div class="mb-3">
+        <Dropdown direction="right" group size="lg">
+          <DropdownToggle color='secondary' caret>{current_example.name}</DropdownToggle>
+          <DropdownMenu class="dropdown-menu-dark">
+            {#each example_list as example_item}
+                <DropdownItem active={current_example.name===example_item.name}
+                    on:click={() => current_example=example_item}>
+                    {example_item.name}
+                </DropdownItem>          
+            {/each}
+          </DropdownMenu>
+        </Dropdown>
+      </div>
 
-        {example.texts.join(default_separator).slice(0, 159)}...
-    
-    </b><p></p>
-    By default (<b>,</b>) uses as separator. But you can change it.<br>
-    Input any text to <b>Separator</b> input if you want.<p></p>
-    When everything is ready, press the <b>Find similar</b> button and enjoy the results.<p></p>
-    We prepared the example for you: -> <Button on:click={() => set_example_value(example)} color="info" type="button">Try example</Button>
-    {:catch error}
-        <p style="color: red">{error.message}</p>
-    {/await}
+    {#if current_example.query_name === 'films-demo'}
+      {#await example}
+      <p>...waiting</p>
+      {:then example}
+      This is the most simple example. <br>You can compare one text to anothers and find similars.
+      <p></p>
+      Input text to compare to the <b>Main text</b> input.<br>
+      Any text will be good. For example: <b>
+          {example.text}
+      </b>.<p></p>
+      Input other texts to compare and find similars to the <b>List of texts</b> input.<br>
+      For example: <b>
+  
+          {example.texts.join(default_separator).slice(0, 159)}...
+      
+      </b><p></p>
+      By default (<b>,</b>) uses as separator. But you can change it.<br>
+      Input any text to <b>Separator</b> input if you want.<p></p>
+      When everything is ready, press the <b>Find similar</b> button and enjoy the results.<p></p>
+      We prepared the example for you: -> <Button on:click={() => set_example_value(example)} color="info" type="button">Try example</Button>
+      {:catch error}
+          <p style="color: red">{error.message}</p>
+      {/await}
+    {:else}
+        <p>Example: <b>{current_example.name}</b><br> will be available soon.</p>
+        <p>You can try use API directly on <a href="http://api.findsimilar.org/" target="_blank">api.findsimilar.org</a>
+             for a while.</p>
+    {/if}
+
 </div>
--- src/routes/tutorial/core.js
+++ src/routes/tutorial/core.js
@@ -1,8 +1,15 @@
 import { dev } from '$app/environment'
 
-export async function fetch_data(url, method, data) {
+const IS_MOCK_RESULTS = true // change it to get responses from server
+
+export async function fetch_data(url, method, data, mocked_result={}) {
+
+        const mock_results = dev ? IS_MOCK_RESULTS : false
+        if (mock_results) {
+            return mocked_result
+        }
+
         const api_url = `api${url}`
-        //const production_url = 'http://188.64.12.238:8000/'
         const production_url = 'http://api.findsimilar.org/'
         const full_url = dev ? `http://127.0.0.1:8000/${api_url}` : `${production_url}${api_url}`
         let params = {
